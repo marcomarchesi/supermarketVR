28727878e90d68a25ed96b0b47e176c277aadd17
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2015-04-01 13:18:05 +0200

    new collisions and chart following camera

--- public/js/DK2Controls.js
+++ public/js/DK2Controls.js
@@ -16,7 +16,8 @@ Copyright 2014 Lars Ivar Hatledal
    @modified by Pierfrancesco Soffritti
 */
 
-var lastNotCollidingPositionX = 0, lastNotCollidingPositionZ = 0;
+// used to handle collisions
+var canMoveUP = 1, canMoveDOWN = 1, canMoveRIGHT = 1, canMoveLEFT =1;
 
 THREE.DK2Controls = function(object) {
 
@@ -90,14 +91,6 @@ THREE.DK2Controls = function(object) {
         case 65: /*A*/
             this.wasd.left = true;
             break;
-
-        // case 82: /*R*/
-        //     this.moveUp = true; 
-        //     break;
-
-        // case 70: /*F*/
-        //     this.moveDown = true;
-        //     break;
       }
 
     }
@@ -123,14 +116,6 @@ THREE.DK2Controls = function(object) {
       case 65: /*A*/
           this.wasd.left = false;
           break;
-
-      // case 82: /*R*/
-      //     this.moveUp = false; 
-      //     break;
-
-      // case 70: /*F*/
-      //     this.moveDown = false;
-      //     break;
     }
   };
 
@@ -149,33 +134,23 @@ THREE.DK2Controls = function(object) {
 
         // collisions, camera and oject are (at the moment) two separate objects
         camera.setRotationFromQuaternion(this.headQuat);
+        //cart_mesh.setRotationFromQuaternion(this.headQuat);
 
         this.controller.setRotationFromMatrix(this.object.matrix);
       }
-
-
 
       this.lastId = id;
     }
 
     // update position TODO here for rotate the cart ???
-    if (this.wasd.up){
-      this.controller.translateZ(-this.translationSpeed * delta * walkingFactor);
-    }
-
+    if (this.wasd.up)
+      this.controller.translateZ(-this.translationSpeed * delta * walkingFactor * canMoveUP);
     if (this.wasd.down)
-      this.controller.translateZ(this.translationSpeed * delta);
-
+      this.controller.translateZ(this.translationSpeed * delta * canMoveDOWN);
     if (this.wasd.right)
-      this.controller.translateX(this.translationSpeed * delta);
-
+      this.controller.translateX(this.translationSpeed * delta * canMoveRIGHT);
     if (this.wasd.left)
-      this.controller.translateX(-this.translationSpeed * delta);
-
-    if (  this.moveUp)
-      this.controller.translateY( this.translationSpeed * delta );
-    if (this.moveDown)
-      this.controller.translateY( - this.translationSpeed * delta );
+      this.controller.translateX(-this.translationSpeed * delta * canMoveLEFT);
 
      //UNDER TEST
       // this.camera.rotation.y = -this.lookSpeed;
@@ -183,17 +158,17 @@ THREE.DK2Controls = function(object) {
 
     // both camera and object (camera's bounding box) need to be updated
     this.object.position.addVectors(this.controller.position, this.headPos);
-    // collisions, camera and oject are (at the moment) two separate objects
     camera.position.addVectors(this.controller.position, this.headPos);
-    
-    handleCollisions(this.object, this.controller, this.wasd.right, this.wasd.left, this.wasd.up, this.wasd.down);
+    cart_mesh.position.addVectors(this.controller.position, this.headPos);
 
-    //if (this.object.position.y < -10) {
-    //   this.object.position.y = -10;
-    // }
-
-    console.log("controller: " +this.controller.position.x +", " +this.controller.position.y  +", " +this.controller.position.z);
-    console.log("object    : " +this.object.position.x +", " +this.object.position.y +", " +this.object.position.z);
+    if(enableCollision) {
+      canMoveLEFT =1;
+      canMoveRIGHT=1;
+      canMoveDOWN=1;
+      canMoveUP=1;
+      
+      handleCollisions(this.controller, this.wasd.right, this.wasd.left, this.wasd.up, this.wasd.down);
+    }
 
       if (ws) {
         if (ws.readyState === 1) {
@@ -206,63 +181,121 @@ THREE.DK2Controls = function(object) {
   window.addEventListener( 'keyup', bind( this, this.onKeyUp ), false );
   
   function bind( scope, fn ) {
-
     return function () {
-
       fn.apply( scope, arguments );
-
     };
 
   };
 
-  var colliding = false;
+  function handleCollisions(controller, right, left, up, down) { 
+    // Set the rays. One vector for every potential direction
+    var rays = [
+      new THREE.Vector3(0, 0, 1),   // up
+      new THREE.Vector3(1, 0, 1),   // up left 
+      new THREE.Vector3(1, 0, 0),   // left
+      new THREE.Vector3(1, 0, -1),  // down left
+      new THREE.Vector3(0, 0, -1),  // down
+      new THREE.Vector3(-1, 0, -1), // down right
+      new THREE.Vector3(-1, 0, 0),  // right
+      new THREE.Vector3(-1, 0, 1)   // up right
+    ];
 
-  function handleCollisions(movingObject, controller, right, left, up, down) {
-    
-    // collision detection:
-    //   determines if any of the rays from the cube's origin to each vertex
-    //   intersects any face of a mesh in the array of target meshes
-    //   for increased collision accuracy, add more vertices to the cube;
-    //   HOWEVER: when the origin of the ray is within the target mesh, collisions do not occur
-    
-    actualPosition = movingObject.position.clone();
-    var objectsArray = new Array();
-    objectsArray.push(boundingBoxMesh);
-    
-    for (var vertexIndex = 0; vertexIndex < movingObject.geometry.vertices.length; vertexIndex++) {   
-      var localVertex = movingObject.geometry.vertices[vertexIndex].clone();
-      var globalVertex = localVertex.applyMatrix4( movingObject.matrix );
-      var directionVector = globalVertex.sub( movingObject.position );
-      
-      var ray = new THREE.Raycaster( actualPosition, directionVector.clone().normalize() );
-      var collisionResults = ray.intersectObjects( objectsArray );
-      
-      // If the distance to an intersection is less than the distance between the Player's position and the geometry's vertex,
-      // then the collision occurred on the interior of the player's mesh -- what we would probably call an "actual" collision.
-      if ( (collisionResults.length > 0) && (collisionResults[0].distance < directionVector.length()) ) {
-        // collision detected
-        console.log("collision");
-        // restore position
-        controller.position.x = lastNotCollidingPositionX;
-        controller.position.z = lastNotCollidingPositionZ;
+    var caster = new THREE.Raycaster();
+    var collisions, i;
 
-        // movingObject.position.x = lastNotCollidingPositionX;
-        // movingObject.position.z = lastNotCollidingPositionZ;
+    // Maximum distance from the origin before we consider collision
+    distance = 0.5;
+    // Get the obstacles array from our world
+    obstacles = new Array();
+    obstacles.push(boundingBoxMesh);
 
-        // console.log("last not colligind pos: " +lastNotCollidingPositionX +"actual pos: " +controller.position.x );
-      } else if ((collisionResults.length > 0) && (collisionResults[0].distance > directionVector.length())) {
-        // no collision detected -> save position
-        if(left)
-          lastNotCollidingPositionX = controller.position.x+0.04;
-        if(right)
-          lastNotCollidingPositionX = controller.position.x-0.01;
-        if(up)
-          lastNotCollidingPositionZ = controller.position.z;
-        if(down)
-          lastNotCollidingPositionZ = controller.position.z;
-        // console.log("last not colligind pos: " +lastNotCollidingPositionX);
+    // For each ray
+    for (i = 0; i < rays.length; i += 1) {
+      // We reset the raycaster to this direction
+      caster.set(controller.position, rays[i]);
+      // Test if we intersect with any obstacle mesh
+      collisions = caster.intersectObjects(obstacles);
+      // And disable that direction if we do
+      if (collisions.length > 0 && collisions[0].distance <= distance) {
+        if ((i === 0 || i === 1 || i === 7) && right) {
+          console.log("collision right");
+          canMoveRIGHT = 0;
+        } else if ((i === 3 || i === 4 || i === 5) && left) {
+          console.log("collision left");
+          canMoveLEFT = 0;
+        }
+        if ((i === 1 || i === 2 || i === 3) && up) {
+          console.log("collision up");
+          canMoveUP = 0;
+        } else if ((i === 5 || i === 6 || i === 7) && down) {
+          console.log("collision down");
+          canMoveDOWN = 0;
+        }
       }
+
+
+      // collisione parte frontale carrello
+      // not working ..
+      /*
+      if (collisions.length > 0 && collisions[0].distance <= distance+2) {
+        if ((i === 1 || i === 2 || i === 3) && up) {
+          console.log("collision up2");
+          canMoveUP = 0;
+        } 
+      }
+
+      */
+
     }
   }
-  
+
+
+  // old method NOT WORKING WELL
+  // function handleCollisions(movingObject, controller, right, left, up, down) {
+    
+  //   // collision detection:
+  //   //   determines if any of the rays from the cube's origin to each vertex
+  //   //   intersects any face of a mesh in the array of target meshes
+  //   //   for increased collision accuracy, add more vertices to the cube;
+  //   //   HOWEVER: when the origin of the ray is within the target mesh, collisions do not occur
+    
+  //   actualPosition = movingObject.position.clone();
+  //   var objectsArray = new Array();
+  //   objectsArray.push(boundingBoxMesh);
+    
+  //   for (var vertexIndex = 0; vertexIndex < movingObject.geometry.vertices.length; vertexIndex++) {   
+  //     var localVertex = movingObject.geometry.vertices[vertexIndex].clone();
+  //     var globalVertex = localVertex.applyMatrix4( movingObject.matrix );
+  //     var directionVector = globalVertex.sub( movingObject.position );
+      
+  //     var ray = new THREE.Raycaster( actualPosition, directionVector.clone().normalize() );
+  //     var collisionResults = ray.intersectObjects( objectsArray );
+      
+  //     // If the distance to an intersection is less than the distance between the Player's position and the geometry's vertex,
+  //     // then the collision occurred on the interior of the player's mesh -- what we would probably call an "actual" collision.
+  //     if ( (collisionResults.length > 0) && (collisionResults[0].distance < directionVector.length()) ) {
+  //       // collision detected
+  //       console.log("collision");
+  //       // restore position
+  //       controller.position.x = lastNotCollidingPositionX;
+  //       controller.position.z = lastNotCollidingPositionZ;
+
+  //       // movingObject.position.x = lastNotCollidingPositionX;
+  //       // movingObject.position.z = lastNotCollidingPositionZ;
+
+  //       // console.log("last not colligind pos: " +lastNotCollidingPositionX +"actual pos: " +controller.position.x );
+  //     } else if ((collisionResults.length > 0) && (collisionResults[0].distance > directionVector.length())) {
+  //       // no collision detected -> save position
+  //       if(left)
+  //         lastNotCollidingPositionX = controller.position.x+0.04;
+  //       if(right)
+  //         lastNotCollidingPositionX = controller.position.x-0.01;
+  //       if(up)
+  //         lastNotCollidingPositionZ = controller.position.z;
+  //       if(down)
+  //         lastNotCollidingPositionZ = controller.position.z;
+  //       // console.log("last not colligind pos: " +lastNotCollidingPositionX);
+  //     }
+  //   }
+  // }  
 };
